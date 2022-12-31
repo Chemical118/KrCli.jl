b875207b5759f4fe6934a5829bf9a85306da68b7
Ryu <wowo0118@sasa.hs.kr>
2022-12-31 15:37:50 +0900

    12/31

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 echo "change test"
 
 if [ ! -f "/root/.julia/bin/krcli" ]; then
-    julia -e 'using KrCli; KrCli.comonicon_install();'
+    exec julia -e 'using KrCli; KrCli.comonicon_install();'
 fi
 
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
