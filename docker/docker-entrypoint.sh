1b8775a5021402b1a05ea5d3aed3ad564fc2f74d
Ryu <wowo0118@sasa.hs.kr>
2022-12-31 19:19:20 +0900

    12/31

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 if [ ! -f "/root/.julia/bin/krcli" ]; then
-    exec julia -e 'using KrCli; KrCli.comonicon_install();' && "$@"
+    julia -e 'using KrCli; KrCli.comonicon_install();'
 fi
 
 exec "$@"
